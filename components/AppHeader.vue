d27c09caf522dd729f487426cfb69e5e3f530db9
Ashlyn <ashlyn@lilyx.io>
2022-03-07 12:48:39 -0700

    initial commit for activity

---
+++ components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+  </header>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'Default Header'
+    }
+  },
+  setup() {
+    const description =
+      'This is a description coming from the script in the Header component.'
+    return {
+      description
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <AppHeader />
+    <section>
+      <h2>Page Content</h2>
+      <p>This section is directly on the index.vue file</p>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
